0bc9aa8b0ec5cbe45b51a7aa1cf501976e4af059
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-05-02 18:05:42 -0600

    Finished

--- instagram.js
+++ instagram.js
@@ -11,15 +11,13 @@ $(document).ready(function()
 	    if(e.which == 13) 
 	    {
 	    	$("input").blur(); //hides keyboard on touch device when enter is hit
-	    	$("button").css("background", "#2F4858"); //changes color of the button when enter is hit, similar to an active/hover
 	        instagram(); //runs instagram function
-	    	setTimeout(function(){ $("button").css('background', ''); },500)
 	    }
 	});
 
 	$("button").on('click', function() //runs instagram function when user hits button
 	{
-		if ('createTouch' in document) //fixes sitcky hover on touch devices
+		if ('createTouch' in document) //fixes sticky hover on touch devices
 		{
 		    try
 		    {
@@ -39,13 +37,12 @@ $(document).ready(function()
 		    }
 		    catch(e){}
 		}
-		$("button").css("background", "#2F4858");
 		instagram(); //runs instagram function
-		setTimeout(function(){ $("button").css('background', ''); },500)
   	});
 
   	function instagram() //finds most liked instagram picture function
 	{
+		$("button").css("background", "#2F4858"); //changes color of the button when enter/button is hit, similar to an active/hover
 	    $("li").remove(); //clears previous picture
 		var user = $('input').val().toLowerCase(); //makes input case insensitive
 		var compare = [];
@@ -54,12 +51,12 @@ $(document).ready(function()
 
 	    if (user == "") //if nothing is entered in input box
 	    {
-	    	confirm("Enter a username!");
+	    	confirm("Enter a user-name!");
 	    }
 
 	    else //if something is entered in input box
 	    {
-		    $.ajax( //uses user input to query instagram usernames
+		    $.ajax( //uses user input to query instagram user-names
 		    {
 		    	type: "GET",
 		      	dataType: "jsonp",
@@ -69,17 +66,17 @@ $(document).ready(function()
 		      	success: function(data)
 		  		{
 		      		// console.log(data.data.length);
-			        if (data.data.length == 0) //no usernames appear
+			        if (data.data.length == 0) //no user-names appear
 			        {
 			        	// console.log("This user name does not exist, 0 came up");
-				        confirm("This username does not exist, check spelling!")
+				        confirm("This user-name does not exist, check spelling!")
 				    }
 				        
-			        else //usernames appear
+			        else //user-names appear
 			        {
-			        	for(i=0; i<data.data.length; i++) //loops through list of usernames
+			        	for(i=0; i<data.data.length; i++) //loops through list of user-names
 			          	{
-			          		if (user == data.data[i].username) //a username match is the same as what the user entered in the input box, match!
+			          		if (user == data.data[i].username) //a user-name match is the same as what the user entered in the input box, match!
 			          		{
 			            		// console.log("if" + data.data[i].username);
 			            		var id = data.data[i].id; //finds the user id we need in order to get recent media
@@ -93,19 +90,19 @@ $(document).ready(function()
 
 					              	success: function(data)
 					              	{
-						                if (data.meta.code == 400) //username is private
+						                if (data.meta.code == 400) //user-name is private
 						                {
 						                	confirm("This user is private!");
 						                }
 							                
-						                else if (data.data.length == 0) //username has no posts
+						                else if (data.data.length == 0) //user-name has no posts
 						                {
 						                  	confirm("This user has no posts!");
 						                }
 							                
 						                else
 						                {
-						                	for (var a = 0; a < data.data.length; a++) //loops through usernames recent media
+						                	for (var a = 0; a < data.data.length; a++) //loops through user-names recent media
 						                	{
 						                  		var likes = data.data[a].likes.count; //finds users likes
 						                  		// console.log(likes);
@@ -116,7 +113,7 @@ $(document).ready(function()
 
 							                for (i = 0; i < compare.length; i++) //loops through array of likes
 							                {
-							                	if (compare[i] > likes) //finds largest number of likes by comparing each like to eachother
+							                	if (compare[i] > likes) //finds largest number of likes by comparing each like to each other
 							                	{
 							                    	likes = compare[i];
 							                    	a = compare.indexOf(likes); //grabs the array position of the largest like in the array compare, so we can use that same position to get the picture corresponding to the largest like
@@ -144,13 +141,13 @@ $(document).ready(function()
 			            		break; //breaks out of the loop
 			          		}
 
-			          		else if (user != data.data[i].username) //if the username is not found
+			          		else if (user != data.data[i].username) //if the user-name is not found
 			          		{
-			          			counter++; //counts everytime a username is not found in the list of the usernames
+			          			counter++; //counts every-time a user-name is not found in the list of the user-names
 
-			          			if (counter == data.data.length) //sees if the counter is the same as the number of usernames in the list, if it is, the username does not exist
+			          			if (counter == data.data.length) //sees if the counter is the same as the number of user-names in the list, if it is, the user-name does not exist
 			          			{
-			          				confirm("This username does not exist, check spelling!");
+			          				confirm("This user-name does not exist, check spelling!");
 			          			}
 			          		}		        
 				        }
@@ -159,6 +156,7 @@ $(document).ready(function()
 		      	}
 		    });
 		}
+		setTimeout(function(){ $("button").css('background', ''); },500) //once the function has run, button returns to original color
 	}
 	var token = "token_here"; //api token here
 });
